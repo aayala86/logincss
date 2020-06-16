40cd4a1dc6d4b91cf41865ec5961b19030ef40c9
Antonio Ayala <antonio.ayala.a@gmail.com>
2020-06-16 18:18:28 -0400

    Login CSS

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+function Jugador() {
+
+  this.nombre = nombre;
+  this.pv = 100;
+  this.sp = 100;
+
+}
+
+var gandalf = new Jugador("Gandalf");
+var legolas = new Jugador("Legolas");
+
+
+
+console.log(gandalf);
+console.log(legolas);
+
+
